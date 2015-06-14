5743ceb3ee4f2b0d3f59afa294f113030e2f52a7
adisak1618 <adisakchaiyakul@gmail.com>
2015-06-14 20:33:58 +0700

    beta v.1
    add function

--- helper/helper.php
+++ helper/helper.php
@@ -5,11 +5,19 @@ defined('_JEXEC') or die;
 
 class ModSmartWelcomeHelper{
 	public static function show($params){
-		$app = JFactory::getAppplication();
+		$app = JFactory::getApplication();
 		$secret = $app->getCfg('secret');
 		$cookie = $app->input->cookie->get('JSW_'.$secret);
-		echo $secret;
-		return true;
+		
+		if(!$cookie){
+			$app->input->cookie->get('JSW_'.$secret);
+			return true;
+		}elseif ($cookie < $params->get('visit_count','5')) {
+			 $app->input->cookie->set('JSW_'.$secret,$cookie+1);
+			 return true;
+		}else{
+			return false;
+		}
 	}
 }
 
---
+++ tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('_JEXEC') or die;
+
+
+
+?>
+<div class="alert alert-success" role="alert">
+      <strong><?php echo $params->get('welcome_message'); ?> </strong> 
+    </div>
+   
